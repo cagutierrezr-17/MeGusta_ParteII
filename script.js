fb03e4e53341967f4e2596d71486a887deb77411
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-09-04 16:59:17 -0300

    Me Gusta - Parte 2
    Tarea Requerida - Me Gusta / Parte 2

---
+++ script.js
@@ -0,0 +1,29 @@
+var count = 9;
+var countElement = document.getElementById("count")
+
+console.log(countElement);
+
+function add1() {
+    count++;
+    countElement.innerText = count + " " + "like(s)"
+}
+
+var like1 = 12;
+var countlike1 = document.getElementById("like1")
+
+console.log(countlike1);
+
+function add2() {
+    like1++;
+    countlike1.innerText = like1 + " " + "like(s)"
+}
+
+var like2 = 9;
+var countlike2 = document.getElementById("like2")
+
+console.log(countlike2);
+
+function add3() {
+    like2++;
+    countlike2.innerText = like2 + " " + "like(s)"
+}
